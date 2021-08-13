2154ada94f2f4860d4f53086d7c3f7feaf2cf8c7
Abdul Muqeet Arshad <abduliscoool@gmail.com>
2021-08-13 16:37:25 +0500

    excluded required fields from article add route..

--- validation/article.js
+++ validation/article.js
@@ -1,14 +1,14 @@
 const yup = require("yup")
 
 exports.addArticleSchema = yup.object({
-    name: yup.string().required(),
-    description: yup.string().required(),
-    image: yup.string().required(),
-    style: yup.string().required(),
-    type: yup.string().required(),
-    availableSizes: yup.string().required(),
-    releaseDate: yup.date().required(),
-    price: yup.number().required()
+    name: yup.string(),
+    description: yup.string(),
+    image: yup.string(),
+    style: yup.string(),
+    type: yup.string(),
+    availableSizes: yup.string(),
+    releaseDate: yup.date(),
+    price: yup.number()
 })
 
 exports.getSingleArticle = yup.object({
